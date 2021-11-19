ccd846e4fb515ff313278699c385eea49bf275fc
A.I <alex05102222@gmail.com>
2021-11-20 00:29:01 +0100

    окончена вёрстка hader и footer

--- app/js/main.js
+++ app/js/main.js
@@ -3,9 +3,16 @@ $(function () {
     $('.dropdown__list').toggleClass('dropdown__list--active');
   });
 
+  $('.dropdown__link').on('click', function () {
+    $('.dropdown__list').removeClass('dropdown__list--active');
+  });
+
+  $('.dropdown__link').on('click', function () {
+    $('.dropdown__btn').removeClass('dropdown__btn--active');
+  });
 
   $('.dropdown__btn').on('click', function () {
     $('.dropdown__btn').toggleClass('dropdown__btn--active');
   });
-  
+
 })
